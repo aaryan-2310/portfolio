d283aa00fe07553f568cb8eb131d1d4ae26ddd17
aaryan-2310 <61946336+aaryan-2310@users.noreply.github.com>
2025-06-10 22:51:02 +0530

    commit

---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'portfolio-button',
+  standalone: true,
+  imports: [MatButtonModule],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss'
+})
+export class ButtonComponent {
+  @Input() label: string = '';
+}
